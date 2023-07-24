c6fd0d80c5cbde52bc571ca4d05087ad7f326809
BMRC23 <127077350+BMRC23@users.noreply.github.com>
2023-07-24 22:36:23 +0800

    Delete TrashHeap.java

--- TrashHeap.java
+++
@@ -1,7 +0,0 @@
-public class TrashHeap implements ConsumableItem {
-    @Override
-    public boolean isEatenBy(Snake snake) {
-        // Check if the snake can eat the trash heap, return true if successful
-        return true;
-    }
-}

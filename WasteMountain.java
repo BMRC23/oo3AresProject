5ac5f0054c8f25051fc258e98981daa5712a1dac
BMRC23 <127077350+BMRC23@users.noreply.github.com>
2023-07-24 22:36:30 +0800

    Delete WasteMountain.java

--- WasteMountain.java
+++
@@ -1,7 +0,0 @@
-public class WasteMountain implements ConsumableItem {
-    @Override
-    public boolean isEatenBy(Snake snake) {
-        // Check if the snake can eat the waste mountain, return true if successful
-        return true;
-    }
-}

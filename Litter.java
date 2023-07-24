fee2845fcae4738f6681c845ecf8034a13597fa1
BMRC23 <127077350+BMRC23@users.noreply.github.com>
2023-07-24 22:36:07 +0800

    Delete Litter.java

--- Litter.java
+++
@@ -1,7 +0,0 @@
-public class Litter implements ConsumableItem {
-    @Override
-    public boolean isEatenBy(Snake snake) {
-        // Check if the snake can eat the litter, return true if successful
-        return true;
-    }
-}
